fcbcce2049e22cc59215ad55070b8ab28e8bbf6b
root <ankushkapoornith@gmail.com>
2023-01-27 14:28:17 +0530

    First Commit

---
+++ pythonfile.py
@@ -0,0 +1,13 @@
+#Sample Python Program to swap two Numbers (Hardcoded).
+#Code Contributed by Ankush Kapoor
+
+x=5
+y=4
+
+temp=x
+x=y
+y=temp
+
+print(f"The value of x: {x}. ")
+print(f"The value of y: {y}. ")
+
